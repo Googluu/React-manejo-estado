c7164f4521528408757bbf5b1dc94aaed46e99dc
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-16 09:45:52 -0500

    Cambios agregados

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useReducer } from 'react';
+import './App.css';
+import { UseState } from './UseState'
+import { UseReducer } from './UseReducer';
+function App() {
+  return (
+    <div className="App">
+      <UseState name="Use State" />
+      <UseReducer name="Use Reducer" />
+    </div>
+  );
+}
+
+export default App;
